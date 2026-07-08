a7a90e8727965ba4c50f3edfc1030397e387eaae
Ram-M1 <playing.life.rama@gmail.com>
2026-07-09 00:19:20 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
      и ускоряем загрузку — что особенно важно при слабом интернете)
 */
 
-const CACHE_NAME = 'focus-cache-v74';
+const CACHE_NAME = 'focus-cache-v75';
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
